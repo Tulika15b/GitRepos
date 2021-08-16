b236de682b8741b50af056e0644650e4c1d2a747
Tulika Baranwal <me.tulika@gmail.com>
2021-08-16 21:59:35 +0530

    Added base datamodel class

---
+++ app/src/main/java/com/tulika/gitrepos/data/GitRepo.kt
@@ -0,0 +1,45 @@
+package com.tulika.gitrepos.data
+
+import androidx.annotation.Nullable
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.google.gson.annotations.SerializedName
+
+@Entity(tableName = "gitRepo")
+data class GitRepo(
+
+    @PrimaryKey
+    @SerializedName("id")
+    val id : String,
+
+    @SerializedName("name")
+    val repoName : String,
+
+    @SerializedName("html_url")
+    val repoUrl : String,
+
+    @SerializedName("description")
+    @Nullable
+    val repoDescr : String?,
+
+    @SerializedName("forks_count")
+    val forks : Int,
+
+    @SerializedName("stargazers_count")
+    val stars : Int,
+
+    @SerializedName("language")
+    @Nullable
+    val repoLang : String?,
+
+    var isExpanded: Boolean = false,
+
+   /* @SerializedName("owner")
+    var owner : Owner*/
+){
+
+}
+
+data class Owner(
+    @SerializedName("avatar_url")
+    val repoIconUrl : String)
